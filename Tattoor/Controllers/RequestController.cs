c9f58c19ed096b95c66c2789c06287b6e94f4efa
Drew Meshreky <dmeshreky@precisiondiscovery.com>
2017-04-21 23:10:05 -0700

    initial checking of project
    partial functionality

---
+++ Tattoor/Controllers/RequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tattoor.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult Artist(string id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Tattoor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tattoor.Startup))]
+namespace Tattoor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
